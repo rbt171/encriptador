b68ed179aafd04b99c7edf2539c5a0235ede6958
Robert Martins <rob3r7@outlook.com>
2023-05-25 20:14:57 -0300

    alteracoes

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-document.getElementById('convert-btn').addEventListener('click', function() {
-    var inputText = document.getElementById('input-text').value;
-    var option = document.getElementById('encrypt-radio').checked ? 'encrypt' : 'decrypt';
-    var outputText = '';
-  
-    if (option === 'encrypt') {
-      outputText = encryptText(inputText);
-    } else {
-      outputText = decryptText(inputText);
-    }
-  
+
+
+  function criptografar(){
+    var text = document.getElementById('input-text');
+    var valor = text.value;
+    var outputText = encriptar(valor)
     document.getElementById('output-text').value = outputText;
-    document.getElementById('copy-btn').style.display = 'inline';
-  });
-  
-  document.getElementById('copy-btn').addEventListener('click', function() {
-    var outputText = document.getElementById('output-text');
-    outputText.select();
-    outputText.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-  });
-  
-  function encryptText(text) {
+    
+    
+  }
+  function desencriptar(){
+    var text = document.getElementById('output-text');
+    var valor = text.value;
+    var inputText = decriptar(valor);
+    document.getElementById('input-text').value = inputText;
+  }
+ 
+  function encriptar(text){
     return text.replace(/e/g, 'enter')
                .replace(/i/g, 'imes')
                .replace(/a/g, 'ai')
                .replace(/o/g, 'ober')
                .replace(/u/g, 'ufat');
   }
-  
-  function decryptText(text) {
+  function decriptar(text){
     return text.replace(/enter/g, 'e')
-               .replace(/imes/g, 'i')
-               .replace(/ai/g, 'a')
-               .replace(/ober/g, 'o')
-               .replace(/ufat/g, 'u');
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
+  }
+  
+  function clean(){
+    document.getElementById('input-text').value = '';
   }
   
